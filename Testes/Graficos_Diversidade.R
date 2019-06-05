92db2ef85db4dc7a52b2960e4c8295845f05ec37
faibiofsf <ferreira06@gmail.com>
2019-06-05 01:01:01 -0300

    Add files via upload

---
+++ Testes/Graficos_Diversidade.R
@@ -0,0 +1,37 @@
+library(stringr)
+alfa <- c("1.0","0.5","0.01")
+beta <- c("6.0","3.0","1.0")
+q <- c("0.5","1.0","0.01")
+ro <- c("0.2","0.1","0.05")
+tamColonia <- c("58")
+iteracoes <- c("2000")
+selecao <- c("0","1")
+problema <- c("brazil27", "brazil58")
+pobSelecaoAleatoria <- c("0.0","0.01")
+
+for(i in alfa){
+	for(j in beta){
+		for(k in q){
+			for(l in ro){
+				for(m in tamColonia){
+					for(n in iteracoes){
+						for(o in selecao){
+							for(p in problema){
+								for(ps in pobSelecaoAleatoria){
+									Origem <- str_c("C:\\Workspace\\ACO_NOVO\\src\\Testes\\Saida_Diversidade_",p,"_saidaMelhorGlobal tamColonia-",m,"_iteracoes-",n,"_selecao-",o,"_alfa-",i,"_beta-",j,"_feromonio-",k,"_ro-",l, "_SA-",ps)
+									Dados.brutos <- read.table(str_c(Origem,".txt"), header = FALSE)
+									Dados <- data.frame(Iteracoes = Dados.brutos$V1, Distancia = Dados.brutos$V2)
+									jpeg(filename = str_c(Origem,".jpg"),width = 960, height = 700,quality = 100, restoreConsole = TRUE)
+									plot(Dados, type = 'l', col='red')
+									dev.off()										
+								}
+							}
+						}		
+					}
+				}
+			}
+		
+		}
+		
+	}
+}
